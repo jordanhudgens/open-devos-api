0aa89e3cea57f064f725bb52284f349eaa7f6855
jordanhudgens <jordan@devcamp.com>
2018-05-22 13:29:19 -0500

    Updated the token auth concern

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,19 +1,16 @@
 class TopicsController < ApplicationController
   before_action :set_topic, only: [:show, :update, :destroy]
 
-  # GET /topics
   def index
     @topics = Topic.all
 
     render json: @topics
   end
 
-  # GET /topics/1
   def show
     render json: @topic
   end
 
-  # POST /topics
   def create
     @topic = Topic.new(topic_params)
 
@@ -24,7 +21,6 @@ class TopicsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /topics/1
   def update
     if @topic.update(topic_params)
       render json: @topic
@@ -33,18 +29,15 @@ class TopicsController < ApplicationController
     end
   end
 
-  # DELETE /topics/1
   def destroy
     @topic.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_topic
       @topic = Topic.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def topic_params
       params.require(:topic).permit(:title)
     end
