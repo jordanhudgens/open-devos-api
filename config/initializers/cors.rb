5fbe69cccb9bb21cce6d3cb50f5e562e34ece532
jordanhudgens <jordan@devcamp.com>
2018-05-22 18:15:40 -0500

    Added another cors endpoint

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
+    origins 'http://www.opendevos.com'
+    resource '*', headers: :any, methods: [:get, :post, :options], credentials: true
+  end
+
+  allow do
     origins 'http://localhost:8080'
     resource '*', headers: :any, methods: [:get, :post, :options], credentials: true
   end
