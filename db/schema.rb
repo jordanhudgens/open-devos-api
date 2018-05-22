727a264552ec7aac4b52ebc542908b8fa49aa07c
jordanhudgens <jordan@devcamp.com>
2018-05-22 13:23:40 -0500

    Created a topics controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_22_171501) do
+ActiveRecord::Schema.define(version: 2018_05_22_181924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
