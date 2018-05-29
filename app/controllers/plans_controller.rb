4378e65abba6c3d7438fc2f1008c28e6fbf7a586
jordanhudgens <jordan@devcamp.com>
2018-05-28 22:27:00 -0500

    Added query for the plans controller

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -12,8 +12,7 @@ class PlansController < ApplicationController
   end
 
   def user_plans
-    puts "X" * 500, params.inspect
-    render json: Topic.all
+    render json: User.find(params['user_id']).plans
   end
 
   def create
