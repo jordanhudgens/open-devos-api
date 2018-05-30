0e20ee1889d4bfef9911650cce6770b21a4e1abf
jordanhudgens <jordan@devcamp.com>
2018-05-30 00:38:51 -0500

    Updated the format for the route for devos

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :plans
   get 'user-plans', to: 'plans#user_plans'
-  resources :devos
+  resources :devos, defaults: { :format => 'json' }
   resources :topics
   resource :auth, only: %i[create]
 end
