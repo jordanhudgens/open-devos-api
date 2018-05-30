060e5aacd2a80b78fb1dfe9aa92b928bb0dc0834
jordanhudgens <jordan@devcamp.com>
2018-05-30 00:31:14 -0500

    Updated cors for allowable http verbs

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,16 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://www.opendevos.com'
-    resource '*', headers: :any, methods: [:get, :post, :options], credentials: true
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
   allow do
     origins 'http://www.opendevos.com'
-    resource '*', headers: :any, methods: [:get, :post, :options], credentials: true
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
   allow do
     origins 'http://localhost:8080'
-    resource '*', headers: :any, methods: [:get, :post, :options], credentials: true
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
