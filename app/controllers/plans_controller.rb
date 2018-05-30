cac0637ac3a22539833acac66023c7dd8b7136a5
jordanhudgens <jordan@devcamp.com>
2018-05-29 22:28:01 -0500

    Updated authorization rules for plans

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,6 @@
 class PlansController < ApplicationController
   before_action :set_plan, only: [:show, :update, :destroy]
+  skip_before_action :authenticate_user, only: [:index, :show]
 
   def index
     @plans = Plan.all
