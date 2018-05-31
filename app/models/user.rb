f0079132dffe70528c004ff116d45d67807e8c5a
jordanhudgens <jordan@devcamp.com>
2018-05-31 12:00:42 -0500

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true
   has_many :plans, dependent: :destroy
+
+  validates_presence_of :email, :password, :password_confirmation
 end
