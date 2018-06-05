5219470978f30aac39253ec28d07e08cfd8acab0
jordanhudgens <jordan@devcamp.com>
2018-06-05 15:42:07 -0500

    Built out initial implementation for registration action

--- app/controllers/auths_controller.rb
+++ app/controllers/auths_controller.rb
@@ -3,13 +3,26 @@ class AuthsController < ApplicationController
 
   def register
     puts "Y" * 500, params.inspect
-    # token_command = AuthenticateUserCommand.call(*params.slice(:user, :password).values)
-    #
-    # if token_command.success?
-    #   render json: { token: token_command.result }
-    # else
-    #   render json: { error: token_command.errors }, status: :unauthorized
-    # end
+    user = User.create!(
+      email: params.slice(:user),
+      password: params.slice(:password),
+      password_confirmation: params.slice(:password_confirmation)
+    )
+
+    puts "Z" * 500, user.inspect
+
+    if user
+      token_command = AuthenticateUserCommand.call(user.email, user.password)
+
+      if token_command.success?
+        render json: { token: token_command.result }
+      else
+        render json: { error: token_command.errors }, status: :unauthorized
+      end
+    else
+      render json: { error: 'Error creating account' }, status: :unprocessible_entity
+    end
+
   end
 
   def login
