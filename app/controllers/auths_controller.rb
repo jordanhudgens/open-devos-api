eff1c0f6c13978c3f947394b512158a11cc0143e
jordanhudgens <jordan@devcamp.com>
2018-06-05 01:44:25 -0500

    Updated route for login and register

--- app/controllers/auths_controller.rb
+++ app/controllers/auths_controller.rb
@@ -1,7 +1,17 @@
 class AuthsController < ApplicationController
   skip_before_action :authenticate_user
 
-  def create
+  def register
+    token_command = AuthenticateUserCommand.call(*params.slice(:user, :password).values)
+
+    if token_command.success?
+      render json: { token: token_command.result }
+    else
+      render json: { error: token_command.errors }, status: :unauthorized
+    end
+  end
+
+  def login
     token_command = AuthenticateUserCommand.call(*params.slice(:user, :password).values)
 
     if token_command.success?
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'user-plans', to: 'plans#user_plans'
   resources :devos
   resources :topics
-  resource :auth, only: %i[create]
+  resource :auth, only: %i[create get]
+
+  post :login, to: 'login#auth'
+  post :register, to: 'register#auth'
 end
