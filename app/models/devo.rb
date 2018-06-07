bbf32c8817dfd524235bb80a377037294cbc297d
jordanhudgens <jordan@devcamp.com>
2018-06-06 23:52:27 -0500

    Added the status enum to the devo and plan models

--- app/models/devo.rb
+++ app/models/devo.rb
@@ -3,5 +3,10 @@ class Devo < ApplicationRecord
   friendly_id :title, use: :slugged
   belongs_to :plan
 
+  enum status: {
+    draft: 0,
+    published: 1
+  }
+
   validates_presence_of :title, :content, :position, :plan_id
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -5,6 +5,11 @@ class Plan < ApplicationRecord
   belongs_to :topic
   has_many :devos, dependent: :destroy
 
+  enum status: {
+    draft: 0,
+    published: 1
+  }
+
   validates_presence_of :title,
                         :user_id,
                         :topic_id,
