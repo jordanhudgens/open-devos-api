70cf02edab4eb069b433edd6090d7a81c729dd93
jordanhudgens <jordan@devcamp.com>
2018-06-16 23:42:11 -0600

    Updated the serializer with the new query

--- app/serializers/plan_serializer.rb
+++ app/serializers/plan_serializer.rb
@@ -1,5 +1,10 @@
 class PlanSerializer < ActiveModel::Serializer
-  attributes :id, :title, :slug, :summary, :status
+  attributes :id,
+             :title,
+             :slug,
+             :summary,
+             :status,
+             :last_published
 
   belongs_to :topic
   belongs_to :user
