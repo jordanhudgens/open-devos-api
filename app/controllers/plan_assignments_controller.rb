67fda6479170cf86e36b5aff15b2a9a768b70f69
jordanhudgens <jordan@devcamp.com>
2018-07-14 23:28:55 -0500

    Updated the filter for the auth method

--- app/controllers/plan_assignments_controller.rb
+++ app/controllers/plan_assignments_controller.rb
@@ -1,5 +1,5 @@
 class PlanAssignmentsController < ApplicationController
-  skip_before_action :authenticate_user, only: [:index, :show]
+  skip_before_action :authenticate_user, only: [:show]
 
   def index
     render json: @current_user.plans
