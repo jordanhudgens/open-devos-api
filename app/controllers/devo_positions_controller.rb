d537e4c9eb8844e8a631f341a94760c3486c5d1a
jordanhudgens <jordan@devcamp.com>
2018-08-04 12:40:09 -0700

    Added the query back in for devos

--- app/controllers/devo_positions_controller.rb
+++ app/controllers/devo_positions_controller.rb
@@ -2,8 +2,8 @@ class DevoPositionsController < ApplicationController
   def update
     devos = Devo.where(id: params[:devos])
 
-    params[:devos].each_with_index do |devo, index|
-      devo.update!(position: index)
+    params[:devos].each_with_index do |devo_index, index|
+      Devo.find(devo_index).update!(position: index)
     end
 
     render json: devos.order('position asc')
