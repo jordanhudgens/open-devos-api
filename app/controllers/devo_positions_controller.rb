a4ea955ef24f372d36e7aebb123d65205f0d94e2
jordanhudgens <jordan@devcamp.com>
2018-08-04 12:04:02 -0700

    Removed debbugging output for the devo sorter

--- app/controllers/devo_positions_controller.rb
+++ app/controllers/devo_positions_controller.rb
@@ -1,6 +1,5 @@
 class DevoPositionsController < ApplicationController
   def update
-    puts "ORDER>>>>>>>>>>>>>>" * 10, params['devos'], "<<<<<<<<<<<<<<<<<<<<<<ORDER"
     params[:devos].each_with_index do |id, index|
       Devo.find(id).update!(position: index)
     end
