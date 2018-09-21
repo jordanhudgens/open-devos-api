e22eb36673be6f37443363fe7de79b456e3c00d5
jordanhudgens <jordan@devcamp.com>
2018-09-21 14:12:30 -0500

    Added changes to the plan serializer

--- app/serializers/plan_serializer.rb
+++ app/serializers/plan_serializer.rb
@@ -5,16 +5,18 @@ class PlanSerializer < ActiveModel::Serializer
              :summary,
              :status,
              :last_published,
-             :featured_image
+             :featured_image,
+             :devos
 
   belongs_to :topic
   belongs_to :user
 
-  has_many :devos do
-    if @instance_options[:published_and_draft]
-      @object.devos
-    else
-      @object.devos.published
-    end
+  def devos
+      puts "SERIALIZER STUFF" * 100, @instance_options.inspect, "SERIALIZER STUFF" * 100
+      if @instance_options[:published_and_draft]
+        @object.devos
+      else
+        @object.devos.published
+      end
   end
 end
