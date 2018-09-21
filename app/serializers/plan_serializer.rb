4c9e2b2118e3371eb6ffb081c64f8e4692290f26
jordanhudgens <jordan@devcamp.com>
2018-09-21 13:52:38 -0500

    Added another param call to options

--- app/serializers/plan_serializer.rb
+++ app/serializers/plan_serializer.rb
@@ -5,17 +5,21 @@ class PlanSerializer < ActiveModel::Serializer
              :summary,
              :status,
              :last_published,
-             :featured_image
+             :featured_image,
+             :some_devos
 
   belongs_to :topic
   belongs_to :user
-  has_many :devos do
-    puts "SERIALIZER STUFF" * 100, instance_options.inspect, "SERIALIZER STUFF" * 100
-    @object.devos
-    # if @instance_options[:published_and_draft]
-    #   @object.devos
-    # else
-    #   @object.devos.published
-    # end
+
+  def some_devos
+      puts "SERIALIZER STUFF" * 100, @instance_options.inspect, "SERIALIZER STUFF" * 100
+      puts "OPTIONS" * 100, @options.inspect, "OPTIONS" * 100
+      puts "@OBJECT" * 100, @object.inspect, "OBJECT" * 100
+      @object.devos
+      # if @instance_options[:published_and_draft]
+      #   @object.devos
+      # else
+      #   @object.devos.published
+      # end
   end
 end
