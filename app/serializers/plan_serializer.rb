9f41f85001e3b2e7e2a2b4b4d29851bd9c68d2a4
jordanhudgens <jordan@devcamp.com>
2018-09-26 19:10:46 -0500

    Moved debugging output

--- app/serializers/plan_serializer.rb
+++ app/serializers/plan_serializer.rb
@@ -12,8 +12,10 @@ class PlanSerializer < ActiveModel::Serializer
 
   has_many :devos do |devo|
     if devo.send(:instance_options)['published_and_draft']
+      puts "-" * 500, devo.send(:instance_options)['published_and_draft']
       @object.devos
     else
+      puts "X" * 500, devo.send(:instance_options)['published_and_draft']
       @object.devos.published
     end
   end
