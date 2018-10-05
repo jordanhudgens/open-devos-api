9ff349d6fa5ed5aaf6967f77700d7fe383a42f90
jordanhudgens <jordan@devcamp.com>
2018-10-04 22:02:52 -0500

    Added serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :slug, :full_name, :avatar_url
+  attributes :id,
+             :email,
+             :slug,
+             :full_name,
+             :avatar_url
 
   has_many :plans
   has_many :bookmarks
