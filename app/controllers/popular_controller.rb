aece317f0e213a7098fd0320a4b38442f3087175
jordanhudgens <jordan@devcamp.com>
2018-10-22 19:23:29 -0500

    Rendered the popular response to JSON

--- app/controllers/popular_controller.rb
+++ app/controllers/popular_controller.rb
@@ -2,6 +2,6 @@ class PopularController < ApplicationController
   skip_before_action :authenticate_user, only: [:index]
 
   def index
-    Plan.order_by_most_recent.limit(9)
+    render json: Plan.order_by_most_recent.limit(9)
   end
 end
