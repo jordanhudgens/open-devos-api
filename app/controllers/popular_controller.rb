b78cc6e86872c3282935da7f81d8d8c66b2d7805
jordanhudgens <jordan@devcamp.com>
2018-10-22 19:46:48 -0500

    Added additional debugging output to the popular controller

--- app/controllers/popular_controller.rb
+++ app/controllers/popular_controller.rb
@@ -2,7 +2,9 @@ class PopularController < ApplicationController
   skip_before_action :authenticate_user, only: [:index]
 
   def index
+    @plans = Plan.order_by_most_recent.limit(9)
     puts "X" * 500, params.inspect, "Y" * 500
-    render json: Plan.order_by_most_recent.limit(9)
+    puts "X" * 500, @plans, "Y" * 500
+    render json: @plans
   end
 end
