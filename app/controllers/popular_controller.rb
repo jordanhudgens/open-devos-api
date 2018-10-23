effa8325407cfb8e1c68888e59862085c12bd1c6
jordanhudgens <jordan@devcamp.com>
2018-10-22 19:50:06 -0500

    Updated the query link

--- app/controllers/popular_controller.rb
+++ app/controllers/popular_controller.rb
@@ -2,9 +2,7 @@ class PopularController < ApplicationController
   skip_before_action :authenticate_user, only: [:index]
 
   def index
-    @plans = Plan.order_by_most_recent.limit(9)
-    puts "X" * 500, params.inspect, "Y" * 500
-    puts "X" * 500, @plans, "Y" * 500
+    @plans = Plan.order_by_most_popular.limit(9)
     render json: @plans
   end
 end
