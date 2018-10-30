535ebd6254551d4bcc6e07939b1a3405703da83a
jordanhudgens <jordan@devcamp.com>
2018-10-29 21:44:16 -0500

    Removed references to the organization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
     if user
       session[:user_id] = user.id
-      render json: { status: :created, logged_in: true, user: user, organization: user.organization }
+      render json: { status: :created, logged_in: true, user: user }
     else
       render json: { status: 500 }
     end
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
   def logged_in
     if @current_user
-      render json: { logged_in: true, user: @current_user, organization: @current_user.organization }
+      render json: { logged_in: true, user: @current_user }
     else
       render json: { logged_in: false }
     end
