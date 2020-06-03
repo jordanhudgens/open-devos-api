37fcfcb26caf121c4473e9686ecc57f8cca19b35
jordanhudgens <jordan@devcamp.com>
2020-06-02 19:06:03 -0700

    Refactored the popular controller

--- app/controllers/popular_controller.rb
+++ app/controllers/popular_controller.rb
@@ -1,8 +1,8 @@
 class PopularController < ApplicationController
-  skip_before_action :check_for_current_user, only: [:index]
+  skip_before_action :check_for_current_user
 
   def index
-    @plans = Plan.order_by_most_popular.limit(9)
-    render json: @plans
+    plans = Plan.order_by_most_popular.limit(9)
+    render json: plans
   end
 end
