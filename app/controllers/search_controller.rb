7d5d6d23fcda943372e7a7bb585b8311652d1946
jordanhudgens <jordan@devcamp.com>
2020-06-12 18:43:07 -0700

    Refactored the search controller so it doesn't use instance variables

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,8 @@ class SearchController < ApplicationController
   skip_before_action :check_for_current_user
 
   def search
-    @devos = Devo.search_by_term(params[:query])
+    devos = Devo.search_by_term(params[:query])
 
-    render json: @devos
+    render json: devos
   end
 end
