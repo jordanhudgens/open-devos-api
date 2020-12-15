d845b68b3f04256352a038020c678d9d27febf7c
Jordan Hudgens <jordan@devcamp.com>
2020-12-15 10:06:39 -0700

    Updated the overriden files

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,26 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'https://www.opendevos.com'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+  allow do
+    origins 'http://www.opendevos.com'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 
-# Read more: https://github.com/cyu/rack-cors
+  allow do
+    origins(/\Ahttps?:\/\/([a-zA-Z\d-]+\.){0,}localhost:\d*\/?\z/)
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+  allow do
+    origins 'http://127.0.0.1:5500'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+
+  allow do
+    origins(/https?:\/\/[\S]+/)
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/popular', to: 'popular#index'
+  resources :bookmarks, except: [:show, :update]
+  resources :random_plans, only: [:index]
+  resources :devo_positions, only: [:update]
+  resources :plans
+  get 'user-plans', to: 'plans#user_plans'
+  resources :devos
+  resources :topics
+  resources :plan_assignments, only: [:create, :index, :update]
+  resources :devo_completions, only: [:create, :index, :destroy]
+  get 'last-plan', to: 'plan_assignments#last_plan'
+  resources :users, only: [:update]
+  get 'profile/:user_slug', to: 'users#profile'
+  get :search, to: 'search#search'
+  resources :authors, only: [:index]
+
+  resources :registrations, only: [:create]
+  resources :sessions, only: [:create]
+  delete :logout, to: 'sessions#logout'
+  get :logged_in, to: 'sessions#logged_in'
+  root to: "pages#home"
 end
