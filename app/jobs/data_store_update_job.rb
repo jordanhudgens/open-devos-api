4dda88209d36b97936cded6f77f1555bad929aac
Jordan Hudgens <jordan@devcamp.com>
2020-12-31 11:43:26 -0700

    Created the data store update job with mock calls

---
+++ app/jobs/data_store_update_job.rb
@@ -0,0 +1,22 @@
+class DataStoreUpdateJob < ApplicationJob
+  queue_as :data_updates
+
+  def perform(user_id: nil, name:)
+    if user_id && user = User.find(user_id)
+      if name == "devos"
+        payload = DevosSerializerService.new(user).run!
+      elsif name == "user_plans"
+        payload = UserPlansSerializerService.new(user).run!
+      end
+    elsif name == "featured_plans"
+      payload = FeaturedPlansSerializerService.new(user).run!
+    end
+
+    if payload
+      user.data_stores.create!(
+        name: name,
+        payload: payload
+      )
+    end
+  end
+end
